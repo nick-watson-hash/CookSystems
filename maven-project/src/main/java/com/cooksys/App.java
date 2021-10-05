6281ba466a0b7a62ecb9d1eac7271f16254886e5
NickW <nicholas.watson@mailfence.com>
2021-10-05 15:21:29 -0700

    Maven Project added & added maven to gitignore

---
+++ maven-project/src/main/java/com/cooksys/App.java
@@ -0,0 +1,13 @@
+package com.cooksys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
